2cf3ca81ed2b2e134a1e2f4e0188eaf0aec42b12
long <109195496+dmeoscoten@users.noreply.github.com>
2024-06-04 12:09:09 +0700

    Create time picker dialog

--- app/src/main/java/com/example/final_project/ToDoEditScreen.kt
+++ app/src/main/java/com/example/final_project/ToDoEditScreen.kt
@@ -1,11 +1,15 @@
 package com.example.final_project
 
 import android.annotation.SuppressLint
+import android.app.TimePickerDialog
+import android.widget.TimePicker
 import android.icu.text.SimpleDateFormat
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
@@ -13,7 +17,6 @@ import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -28,9 +31,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
+import androidx.compose.ui.platform.LocalContext
 
 @SuppressLint("UnrememberedMutableState")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,74 +42,98 @@ import java.util.Locale
 fun TodoEditScreen(
     vModel: TodoDetailViewModel = viewModel(factory = AppViewModels.Factory)
 ) {
-    var dateResult by remember { mutableStateOf("Date")}
-    val openDialog = remember {
-        mutableStateOf(false)
-    }
+    var dateResult by remember { mutableStateOf("Date") }
+    val opendateDialog = remember { mutableStateOf(false) }
+    var timeResult by remember { mutableStateOf("Time") }
+    val opentimeDialog = remember { mutableStateOf(false) }
     val state by vModel.state.collectAsState()
+    val context = LocalContext.current
+    var dropdownExpanded by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
-        TextField(value = state.name,
+        TextField(
+            value = state.name,
             onValueChange = { vModel.setName(it) },
-            label = { Text(text = "Name")
-            })
-        TextField(value = state.description,
+            label = { Text(text = "Name") }
+        )
+        TextField(
+            value = state.description,
             onValueChange = { vModel.setdescription(it) },
-            label = { Text(text = "Description")
-            })
-        Button(onClick = { openDialog.value = true }) {
+            label = { Text(text = "Description") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { opendateDialog.value = true }) {
             Text(dateResult)
         }
-//        DropdownMenu(
-//            expanded = true,
-//            onDismissRequest = { /*TODO*/ }
-//        ) {
-//            DropdownMenuItem(onClick = { }) {
-//            }
-//            DropdownMenuItem(onClick = {  }) {
-//            }
-//            DropdownMenuItem(onClick = {  }) {
-//            }
-//        }
+        if (opendateDialog.value) {
+            val datePickerState = rememberDatePickerState()
+            val confirmEnabled = derivedStateOf { datePickerState.selectedDateMillis != null }
+            DatePickerDialog(
+                onDismissRequest = { opendateDialog.value = false },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            opendateDialog.value = false
+                            dateResult = datePickerState.selectedDateMillis?.let {
+                                convertLongToDateString(it)
+                            } ?: "No selection"
+                            vModel.setdate(dateResult)
+                        },
+                        enabled = confirmEnabled.value
+                    ) {
+                        Text(text = "Select")
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            opendateDialog.value = false
+                        }
+                    ) {
+                        Text(text = "Cancel")
+                    }
+                }
+            ) {
+                DatePicker(state = datePickerState)
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { opentimeDialog.value = true }) {
+            Text(timeResult)
+        }
+        if (opentimeDialog.value) {
+            val calendar = Calendar.getInstance()
+            val hour = calendar.get(Calendar.HOUR_OF_DAY)
+            val minute = calendar.get(Calendar.MINUTE)
+
+            TimePickerDialog(
+                context,
+                { _: TimePicker, selectedHour: Int, selectedMinute: Int ->
+                    timeResult = String.format("%02d:%02d", selectedHour, selectedMinute)
+                    opentimeDialog.value = false
+                    vModel.settime(timeResult)
+                },
+                hour,
+                minute,
+                true,
+            ).show()
+        }
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Button(onClick = {  }) {
+            Button(onClick = { /* Handle cancel action */ }) {
                 Text("Cancel")
             }
-            Button(onClick = { vModel.add()}) {
+            Button(onClick = { vModel.add() }) {
                 Text("Add")
             }
         }
-        if(openDialog.value){
-            val datePickerState = rememberDatePickerState()
-            val confirmEndabled =
-                derivedStateOf { datePickerState.selectedDateMillis != null }
 
-            DatePickerDialog(
-                onDismissRequest = { openDialog.value = false },
-                confirmButton = {
-                    TextButton(
-                        onClick = { openDialog.value = false
-                            var date  = "No selection"
-                            if(datePickerState.selectedDateMillis != null){
-                                date = convertLongToDateString(datePickerState.selectedDateMillis!!)
-                            }
-                            dateResult = date
-                            vModel.setdate(dateResult)
-                        },
-                        enabled = confirmEndabled.value
-                        ) {
-                        Text(text = "Select")
-                    }
-                }) {
-                    DatePicker(state = datePickerState)
-            }
-        }
     }
 }
+
 fun convertLongToDateString(timeInMillis: Long): String {
     val date = Date(timeInMillis)
     val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
